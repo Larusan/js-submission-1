6e6d831dad686a4c8d77475efa1c1f63e8c2edf7
Larusan <101088874+Larusan@users.noreply.github.com>
2024-06-06 23:38:38 +0530

    Question 16

---
+++ question16.js
@@ -0,0 +1,14 @@
+// Question 16
+
+function inventoryValue(products) {
+  return products.reduce((totalValue, product) => {
+    return (totalValue = totalValue + product.price * product.quantity);
+  }, 0);
+}
+
+const products = [
+  { name: "Laptop", price: 1000, quantity: 5 },
+  { name: "Phone", price: 500, quantity: 10 },
+];
+
+console.log(inventoryValue(products)); // 10000
